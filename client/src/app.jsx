19e8c8af37abc8a1687e99c4746494eb0a09398b
yty <339355129@qq.com>
2020-03-07 20:42:28 +0800

    xcx初始化

---
+++ client/src/app.jsx
@@ -0,0 +1,70 @@
+import Taro, { Component } from '@tarojs/taro'
+import Index from './pages/common/index/index'
+
+import './app.styl'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+class App extends Component {
+  
+  componentDidMount () {
+    if (process.env.TARO_ENV === 'weapp') {
+      Taro.cloud.init()
+    }
+  }
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  config = {
+    pages: [
+      'pages/common/index/index'
+    ],
+    subPackages: [
+      {
+        root: 'pages/customer',
+        pages: [
+          'index/index'
+        ]
+      },
+      {
+        root: 'pages/business',
+        pages: [
+          'index/index'
+        ]
+      },
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+    cloud: true,
+    functionalPages: {
+      'independent': true
+    },
+    permission: {
+      'scope.userLocation': {
+        desc: '你的位置信息将用于小程序位置接口的效果展示'
+      }
+    }
+  }
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Index />
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
